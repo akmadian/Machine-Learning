7173e13a9e966b7d5fa47b9893b7951d1268e557
Ari Madian <akmadian@gmail.com>
2017-06-07 23:26:32 -0700

    Updated graphing.py
     - Changed plotting module from Matplotlib to Bokeh.
     - Added more data computation features for graph display.
    
     - This script is now Node.JS dependent.

--- graphing.py
+++ graphing.py
@@ -8,19 +8,27 @@
     Repo: github.com/akmadian/Machine-Learning
 """
 
-import matplotlib.pyplot as plt
 import csv
 import arrow
-import time
+import statistics
 from GOLD import patterns
+from bokeh.plotting import figure, output_file, show, save
+from bokeh.layouts import layout
+from bokeh.models import HoverTool, PanTool, ResizeTool, WheelZoomTool, SaveTool, BoxSelectTool, CustomJS, ColumnDataSource
+from bokeh.models.widgets import Toggle, Div
 
 values = []
 csv_headers = ('entryno.', 'accuracy/problist/guesslist/toline',
                'lines_ava', 'time')
 entryno_counter = 0
 run_count = 0
-mpl_x = []
-mpl_y = []
+highest_accuracy = 0
+lowest_accuracy = 100
+avg_accuracy = 0
+total_lines = 0
+bokeh_x = []
+bokeh_y = []
+bokeh_uod = []
 
 
 def time_():
@@ -30,7 +38,7 @@ def time_():
 
 
 def csv_init():
-    with open('Graphing_accuracy_data_0001.csv', 'w') as f:
+    with open('Graphing_accuracy_data_0002.csv', 'w') as f:
         csv.writer(f).writerow(csv_headers)
 
 
@@ -38,39 +46,82 @@ def csv_write(returned):
     del values[:]
     global entryno_counter
     global run_count
+    global highest_accuracy
+    global lowest_accuracy
+    global total_lines
+    global avg_accuracy
     entryno_counter += 1
     run_count += 1
-    with open('Graphing_accuracy_data_0001.csv', 'a') as f:
+    with open('Graphing_accuracy_data_0002.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         values.append(entryno_counter)
         values.append(returned)
         values.append(str(returned[1][0] + returned[1][1]))
         values.append(str(time_()))
 
-        mpl_x.append(run_count)
-        mpl_y.append(returned[0])
+        bokeh_x.append(run_count)
+        bokeh_y.append(returned[0])
+
+        total_lines = returned[3]
+
+        avg_accuracy = float(statistics.mean(bokeh_y))
+
+        if returned[0] > highest_accuracy:
+            highest_accuracy = returned[0]
+
+        if returned[0] < lowest_accuracy:
+            lowest_accuracy = returned[0]
 
         writer.writerow(values)
 
 
-def mpl():
-    file_name = ''.join(['Accuracy_graph_2_' + str(run_count) + '.png'])
-    plt.plot(mpl_x, mpl_y)
-    plt.scatter(mpl_x, mpl_y)
-    plt.xlabel('Evaluation Num')
-    plt.ylabel('Accuracy')
-    plt.savefig(file_name, dpi=200)
+def bokeh():
+    try:
+        file_name = ''.join(['Accuracy_graph_5_' + str(run_count) + '.html'])
 
+        source = ColumnDataSource(data=dict(x=bokeh_x, y=bokeh_y))
 
-csv_init()
+        hover = HoverTool(tooltips=[
+            ('Index', '$index'),
+            ('Accuracy', '@y' + '%'),
+            ('Run Number', '@x'), ])
+
+        TOOLS = [PanTool(), BoxSelectTool(), WheelZoomTool(), hover, SaveTool(), ResizeTool()]
+        p = figure(plot_width=1200, plot_height=450, title='Accuracy By Algorithm Run', tools=TOOLS)
+
+        # HTML For data description below graph
+        div = Div(text=""" <b>Guess Accuracy Graph</b>. <br /><br />
+        Highest Accuracy On Graph - <nobr><b><i>""" + str(highest_accuracy) + """</i></b> % </nobr><br />""" + """
+        Lowest Accuracy On Graph  - <nobr><b><i>""" + str(lowest_accuracy) + """</i></b> % </nobr><br />""" + """
+        Average Accuracy - <nobr><b><i>""" + str(avg_accuracy) + """</i></b> % </nobr><br />""" + """
+        Total Lines Used - <nobr><b><i>""" + str(total_lines) + """</i></b></nobr><br />"""
+                  , width=200, height=100)
+
+        p.circle(x='x', y='y', fill_color='white', size=8, source=source)
+        line = p.line(x='x', y='y', line_width=2, line_alpha=0.5, source=source)
+
+        p.yaxis.axis_label = 'Accuracy %'
+        p.xaxis.axis_label = '# Of guess algorithm runs'
+
+        # JS for button callback handling
+        code = '''object.visible = toggle.active'''
+        callback = CustomJS.from_coffeescript(code=code, args={})
+        toggle = Toggle(label='Toggle Line', button_type='success', callback=callback)
+        callback.args = {'toggle': toggle, 'object': line}
+
+        output_file(file_name)
+
+        show(layout([p], [div, toggle]))
+        save(layout([p], [div, toggle]))
+    except RuntimeError:
+        pass
+
 while True:
     print('start')
     returned_list = patterns.csv_read()
     csv_write(returned_list)
-    mpl()
     print('done')
-    time.sleep(5)
-
-
-
-
+    print(run_count)
+    if run_count % 100 == 0:
+        print('running bokeh')
+        bokeh()
