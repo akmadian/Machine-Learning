253b216cd9acc74714a08fe633d86d8e618bb020
Ari Madian <akmadian@gmail.com>
2017-06-21 22:35:47 -0700

    Updated __main__.py
     - Overhauled write data structure to remove duplicates.
     - Migrated all timestamp handling to the arrow module.
     - Changed output() to improve efficiency.
     - Removed is_connected().
     - Removed redundant variable creation.
     - Changed kwarg handling in local_log().
     - Improved exception handling in scrape().
     - Removed OtherDataGet class because it was useless, also removed most functions from OtherDataGet in favor of syntactically simpler functions, moved methods to global namespace.
     - Added istrading function to prevent bad data along with a check to see if scrape should actually run.
     - Removed the csvops class because the class syntax was useless and moved methods to global namespace.
     - Added argsinterpret to add sys.argv support.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
     File Name: __main__.py
     Author: Ari Madian
@@ -12,7 +13,8 @@ import requests
 import csv
 import time
 import arrow
-import sys, traceback
+import sys
+import traceback
 import smtplib
 import configparser
 from string import Template
@@ -20,54 +22,34 @@ from twilio.rest import Client
 import os
 import socket
 
-csv_headers = ['entryno.', 'value', 'UOD',
+csv_headers = ['entryno.', 'value',
                'time', 'DOW', 'timeperiod',
                'OUDstreaktype', 'OUDstreakno.']
 values = [0, 0, 0, 0, 0, 0, 0, 0]
 values_cache = [0, 0, 0, 0, 0, 0, 0, 0]
 entryno_counter = 1
-write_file_name = 'COMMODITIES_GOLD_DATA_0011.csv'
+write_file_name = 'COMMODITIES_GOLD_DATA_test.csv'
 
 GREEN = '\033[0;32m'
 RESET = '\033[0;0m'
 RED = '\033[0;31m'
 
 
-def is_connected():
-    """Checks to see if an internet connection is active"""
-    try:
-        host = socket.gethostbyname('www.google.com')
-        s = socket.create_connection((host, 80), 2)
-        return True
-    except:
-        pass
-    return False
-
-
-def output(run_status, statuscode):
+def output(statuscode):
     """ Outputs console-like information about the program runtime"""
     sys.stdout.write('__main__.py' + '\n')
     # Status
     print('\r' + 'Status        - [', end='')
-    if run_status == 1:  # Good
-        print('\r' + 'Status        - [', end='')
-        sys.stdout.write(GREEN)
-        print('OK', end='')
-        sys.stdout.write(RESET)
-        sys.stdout.write(']' + '\n')
-    elif run_status == 2:  # Stopped
-        sys.stdout.write('\033[F')
-        sys.stdout.flush()
-        print('\r' + 'Status        - [', end='')
-        sys.stdout.write(RED)
-        print('STOPPED', end='')
-        sys.stdout.write(RESET)
-        sys.stdout.write(']' + '\n')
+
+    print('\r' + 'Status        - [', end='')
+    sys.stdout.write(GREEN)
+    print('OK', end='')
+    sys.stdout.write(RESET)
+    sys.stdout.write(']' + '\n')
     print('Time          - [' + str(arrow_timestamp()) + ']') # Time
-    print('Response Code - [' + str(statuscode) + ']') # Response Code
-    print('Num Of Values - [' + str(entryno_counter) + ']') # Num of values written
-    print('PID           - [' + str(os.getpid()) + ']') # Process ID
-    print('Internet Conn - [' + str(is_connected()) + ']') # Connected To Internet
+    print('Response Code - [' + str(statuscode) + ']')  # Response Code
+    print('Num Of Values - [' + str(entryno_counter) + ']')  # Num of values written
+    print('PID           - [' + str(os.getpid()) + ']')  # Process ID
     print('\n')
 
 
@@ -140,8 +122,7 @@ def text_log(**kwargs):
     See kwargs: in email() for kwargs docs
     :return:
     """
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    config = configparser.ConfigParser().read('config.ini')
     account_sid = config['BUGREPORTING']['twilioaccssid']
     auth_token = config['BUGREPORTING']['twilioauthtoken']
     client = Client(account_sid, auth_token)
@@ -164,9 +145,7 @@ Exception Type..: $extype
     print('Text Log Successful')
 
 
-def local_log(extype= None, statuscode= None, traceback= None,
-              dir= None, values= None, programname= None, time=None,
-              addinfo= None, fname=None):
+def local_log(**kwargs):
     """ Logs an exception/ error report locally
 
     Longer description is essentially the same as email() and text_log()
@@ -177,7 +156,14 @@ def local_log(extype= None, statuscode= None, traceback= None,
     See kwargs: in email() for the rest of them
     :return:
     """
-    filename = fname + '.txt'
+    args = ('exctype', 'statuscode', 'traceback',
+            'dir', 'values', 'programname',
+            'time', 'addinfo')
+    for i in args:
+        if i not in kwargs:
+            kwargs[i] = None
+
+    filename = kwargs['fname'] + '.txt'
     '''
     with open(filename, 'w') as f:
         for arg in argslist:
@@ -218,226 +204,210 @@ def scrape():
         page = session.get(stock_site, headers={'User-Agent': 'Mozilla/5.0'})
         code = str(page.status_code)
         try:
-            assert 200 == 200
+            assert 200 <= int(code) < 300
         except AssertionError as e:
-            output(2, code)
-            print(e)
-            email(extype='AssertionError', statuscode=code, traceback=e,
-                  values=[values, values_cache], programname='__main__.py/ COMMODITIES_GOLD.py',
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            exc_mes = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
+            email(extype='AssertionError', statuscode=code, traceback=exc_mes,
+                  values=[values, values_cache], programname='Machine-Learning/__main__.py',
                   addinfo='Bad status code.', time=arrow_timestamp())
             local_log(fname='__main__errlog_run_0004', extype='AssertionError',
-                      statuscode=code, traceback=e, programname='__main__.py/ COMMODITIES_GOLD.py',
+                      statuscode=code, traceback=e, programname='Machine-Learning/__main__.py',
                       addinfo='Bad status code')
-            text_log(time=arrow_timestamp(), programname='__main__.py/ COMMODITIES_GOLD.py', extype='AssertionError')
+            text_log(time=arrow_timestamp(), programname='Machine-Learning/__main__.py',
+                     extype='AssertionError')
             raise AssertionError
         else:
             os.system('cls')
-            output(1, code)
+            output(code)
             print(values)
             print(values_cache)
+
             tree = html.fromstring(page.content)
-            n_converted = tree.xpath(stock_xpath)
-            s_converted = n_converted[0].text
-            for letter in s_converted:
+            value = tree.xpath(stock_xpath)[0].text
+            for letter in value:
                 if letter.isdigit():
                     value_.append(letter)
                 elif letter == '\'' or letter == ',' or letter == '.':
                     pass
-                nvalue = ''.join(value_)
-            values.append(nvalue)
-            CSVOps.csv_write(code)
+            values.append(int(''.join(value_)))
+            csv_write(code)
 
-    except requests.exceptions.Timeout as e:
-        output(2, code)
+    except requests.exceptions.Timeout:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         exc_mes = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
-        email(extype='requests.exceptions.ConnectionError', statuscode=code,
-              traceback=exc_mes, values=[values, values_cache], programname='__main__.py/ COMMODITIES_GOLD.py')
+        try:
+            email(extype='requests.exceptions.ConnectionError', statuscode=code,
+                  traceback=exc_mes, values=[values, values_cache],
+                  programname='Machine-Learning/__main__.py')
+        except NameError:
+            email(extype='requests.exceptions.ConnectionError', traceback=exc_mes,
+                  values=[values, values_cache], programname='Machine-Learning/__main__.py')
         local_log(fname='__main__errlog_run_0004', extype='requests.exceptions.ConnectionError',
-                  statuscode=code, traceback=exc_mes, programname='__main__.py/ COMMODITIES_GOLD.py',
+                  statuscode=code, traceback=exc_mes, programname='Machine-Learning/__main__.py',
                   addinfo='Connection error, see traceback')
-        text_log(time=arrow_timestamp(), programname='__main__.py/ COMMODITIES_GOLD.py', extype='requests.exceptions.ConnectionError')
+        text_log(time=arrow_timestamp(), programname='Machine-Learning/__main__.py',
+                 extype='requests.exceptions.ConnectionError')
         time.sleep(900)
         scrape()
         print(exc_mes)
 
     except requests.exceptions.ConnectionError:
-        output(2, code)
         exc_type, exc_value, exc_traceback = sys.exc_info()
         exc_mes = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
-        email(extype='requests.exceptions.ConnectionError', statuscode=code,
-              traceback=exc_mes, values=[values, values_cache], programname='__main__.py/ COMMODITIES_GOLD.py')
+        try:
+            email(extype='requests.exceptions.ConnectionError', statuscode=code,
+                  traceback=exc_mes, values=[values, values_cache],
+                  programname='__main__.py')
+        except NameError:
+            email(extype='requests.exceptions.ConnectionError',
+                  traceback=exc_mes, values=[values, values_cache],
+                  programname='__main__.py')
+
         local_log(fname='__main__errlog_run_0004', extype='requests.exceptions.ConnectionError',
-                  statuscode=code, traceback=exc_mes, programname='__main__.py/ COMMODITIES_GOLD.py',
+                  statuscode=code, traceback=exc_mes, programname='__main__.py',
                   addinfo='Connection error, see traceback')
-        text_log(time=arrow_timestamp(), programname='__main__.py/ COMMODITIES_GOLD.py', extype='requests.exceptions.ConnectionError')
+        text_log(time=arrow_timestamp(), programname='__main__.py',
+                 extype='requests.exceptions.ConnectionError')
         time.sleep(900)
         scrape()
         print(exc_mes)
 
     except requests.exceptions.RequestException as e:
-        output(2, code)
         print(e)
         exc_type, exc_value, exc_traceback = sys.exc_info()
         exc_mes = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
-        email(extype='requests.exceptions.RequestException', statuscode=code,
-              traceback=exc_mes, values=[values, values_cache], programname='__main__.py')
-        local_log(fname='__main__errlog_run_0004', extype='requests.exceptions.RequestException',
-                  statuscode=code, traceback=exc_mes, programname='__main__.py/ COMMODITIES_GOLD.py',
-                  addinfo='Catchall requests error, see traceback')
-        text_log(time=arrow_timestamp(), programname='__main__.py/ COMMODITIES_GOLD.py', extype='requests.exceptions.RequestException')
-
-
-class OtherDataGet:
-    """ Gets and assembles other types of data for the log file"""
-    streak = 0
-    streak_type = 3
-    uod_state = 3
-
-    @staticmethod
-    def uod():
-        """ Determines whether the last value was higher, lower, or the same
-            as the one before it.
-
-        :return int:    See main file docstring for int key
-        """
-        global values_cache
         try:
-            if int(values[1]) > int(values_cache[1]):  # If value went up
-                if OtherDataGet.streak_type != 0:  # If it was previously down or same
-                    OtherDataGet.uod_state = 0  # Set state to up
-                    OtherDataGet.streak = 0  # Reset streak
-                    OtherDataGet.streak_type = 0  # Set streak type to up
-                if int(values_cache[4]) == 0:  # If last state was up
-                    OtherDataGet.streak += 1  # Add one to streak counter
-                return 0
-            elif int(values[1]) < int(values_cache[1]):  # If value went down
-                if OtherDataGet.streak_type != 1:
-                    OtherDataGet.uod_state = 1
-                    OtherDataGet.streak = 0
-                    OtherDataGet.streak_type = 1
-                if int(values_cache[4]) == 1:
-                    OtherDataGet.streak += 1
-                return 1
-            elif int(values[1]) == int(values_cache[1]):  # If value is the same
-                if OtherDataGet.streak_type != 2:
-                    OtherDataGet.uod_state = 2
-                    OtherDataGet.streak = 0
-                    OtherDataGet.streak_type = 2
-                if int(values_cache[4]) == 2:
-                    OtherDataGet.streak += 1
-                return 2
-        except IndexError:
-            try:
-                values_cache = [0, 0, 0, 0, 0, 0, 0]
-                OtherDataGet.uod()
-            except RecursionError:
-                pass
+            email(extype='requests.exceptions.RequestException', statuscode=code,
+                  traceback=exc_mes, values=[values, values_cache], programname='__main__.py')
+        except NameError:
+            email(extype='requests.exceptions.RequestException', traceback=exc_mes,
+                  values=[values, values_cache], programname='__main__.py')
+        local_log(fname='__main__errlog_run_0004', extype='requests.exceptions.RequestException',
+                  statuscode=code, traceback=exc_mes, programname='__main__.py',
+                  addinfo='Catchall requests error, see traceback')
+        text_log(time=arrow_timestamp(), programname='__main__.py/ COMMODITIES_GOLD.py',
+                 extype='requests.exceptions.RequestException')
 
-    @staticmethod
-    def day_of_week():
-        """ Gets the day of the week
 
-        :return int:    See docstring at top of file for int key
-        """
-        list_ = list(time.localtime())
-        day = list_[6] + 1
-        L = ['0', str(day)]
-        return ''.join(L)
+streak = 0
+streak_type = 3
+uod_state = 3
 
-    @staticmethod
-    def time_period():
-        """ Gets the time period
 
-        :return int:    year/mo/dow/12hr/6hr/3hr/1hr
-        """
-        tmp_list = []
-        time_list_ = list(time.localtime())
-        year = time_list_[0]
-        month = time_list_[1]
-        hour = int(time.strftime(format('%H')))
-        tmp_list.append(str(year))
-        tmp_list.append(''.join(['0', str(month)]))
-        tmp_list.append(str(OtherDataGet.day_of_week()))
-        if hour <= 12:
-            tmp_list.append('01')
-            if hour <= 6:
-                tmp_list.append('01')
-                if hour <= 3:
-                    tmp_list.append('01')
-                elif 3 < hour <= 6:
-                    tmp_list.append('02')
-                elif 6 < hour <= 9:
-                    tmp_list.append('03')
-                elif 9 < hour <= 12:
-                    tmp_list.append('04')
-            elif 6 < hour <= 12:
-                tmp_list.append('02')
-        elif hour > 12:
-            tmp_list.append('02')
-            if 12 < hour <= 18:
-                tmp_list.append('03')
-                if 12 < hour <= 15:
-                    tmp_list.append('05')
-                elif 15 < hour <= 18:
-                    tmp_list.append('06')
-                elif 18 < hour <= 21:
-                    tmp_list.append('07')
-                elif 21 < hour <= 24:
-                    tmp_list.append('08')
-            elif 18 < hour <= 24:
-                tmp_list.append('04')
-        tmp_list.append(str(hour))
-        nlist = ''.join(tmp_list)
-        return nlist
+def uod():
+    """ Determines whether the last value was higher, lower, or the same
+        as the one before it.
+
+    :return int:    See main file docstring for int key
+    """
+    global values_cache
+    global streak_type
+    global uod_state
+    global streak
+    try:
+        if int(values[1]) > int(values_cache[1]):  # If value went up
+            if streak_type != 0:  # If it was previously down or same
+                uod_state = 0  # Set state to up
+                streak = 0  # Reset streak
+                streak_type = 0  # Set streak type to up
+            if int(values_cache[4]) == 0:  # If last state was up
+                streak += 1  # Add one to streak counter
+            return 0
+        elif int(values[1]) < int(values_cache[1]):  # If value went down
+            if streak_type != 1:
+                uod_state = 1
+                streak = 0
+                streak_type = 1
+            if int(values_cache[4]) == 1:
+                streak += 1
+            return 1
+        elif int(values[1]) == int(values_cache[1]):  # If value is the same
+            if streak_type != 2:
+                uod_state = 2
+                streak = 0
+                streak_type = 2
+            if int(values_cache[4]) == 2:
+                streak += 1
+            return 2
+    except IndexError:
+        try:
+            values_cache = [0, 0, 0, 0, 0, 0, 0]
+            uod()
+        except RecursionError:
+            pass
+
+
+def istrading():
+    """ Determines if the script is running within the stock's trading hours"""
+    dow = arrow.utcnow().to('US/Pacific').format('dddd')
+    time_ = arrow.utcnow().to('US/Pacific').format('HHmm')
+    if dow != 'Saturday' and 1500 > time_ > 1415:
+        return True
+    else:
+        return False
+
+
+def time_period():
+    """Gets a custom timestamp"""
+    return arrow.utcnow().to('US/Pacific').format('YYYYMMDDdHHmmss')
 
 
 def arrow_timestamp():
+    """Gets a generic timestamp"""
     stamp = arrow.utcnow().to('US/Pacific').format('YYYY-MM-DD HH:mm:ss')
     return stamp
 
 
-class CSVOps:
-    """ Operations involving CSV files"""
-    @staticmethod
-    def csv_write(code):
-        """ Writes to a csv file
+def csv_write(code):
+    """ Writes to a csv file
 
-        Gets additional information and writes it to a CSV file
-        :return:
-        """
-        values.append(arrow_timestamp())
-        values.append(int(OtherDataGet.time_period()))
-        values.append(OtherDataGet.uod())
-        values.append(OtherDataGet.streak)
-        os.system('cls')
-        output(1, code)
-        print(values_cache)
-        print(values)
-        with open(write_file_name, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(values)
-        print(' - Written')
+    Gets additional information and writes it to a CSV file
+    """
+    values.append(arrow_timestamp())
+    values.append(int(time_period()))
+    values.append(uod())
+    values.append(streak)
+    os.system('cls')
+    output(code)
+    print(values_cache)
+    print(values)
+    with open(write_file_name, 'a', newline='') as file:
+        csv.writer(file).writerow(values)
+    print(' - Written')
 
-    @staticmethod
-    def csv_init():
-        """ Initializes a csv file with headers
 
-        :return:
-        """
-        with open(write_file_name, 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(csv_headers)
+def csv_init():
+    """ Initializes a csv file with headers"""
+    cases = [[None, None, None, None, 0, 2],
+             [None, None, None, None, 1, 2],
+             [None, None, None, None, 2, 2]]
+    with open(write_file_name, 'w') as file:
+        csv.writer(file).writerow(csv_headers)
+        for list_ in cases:
+            csv.writer(file).writerow(list_)
+
+
+def argsinterpret(listchoice):
+    rawargs = sys.argv
+    addargs = sys.argv[2:]
+    groupedargs = [''.join([thing, next(iter(rawargs))])
+                   if 'test' in thing else None for thing in iter(rawargs)]
+    return rawargs if listchoice == 'rawargs' \
+        else addargs if listchoice == 'addargs' \
+        else groupedargs if listchoice == 'groupedargs' \
+        else 'Not a valid option'
 
 
 if __name__ == '__main__':
-    output(1, None)
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    output(None)
+    config = configparser.ConfigParser().read('config.ini')
     print(' - Config File Parsed')
-    time.sleep(1)
     print(' - CSV File Initiated')
-    time.sleep(1)
     while True:
-        scrape()
-        entryno_counter += 1
+        if istrading():
+            scrape()
+            entryno_counter += 1
+        else:
+            time.sleep(300)
+            continue
